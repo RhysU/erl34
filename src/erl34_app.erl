b286af36174dfa3a2a9f7a03e93866fb3bbaa2d7
Rhys Ulerich <rhys.ulerich@gmail.com>
2016-11-29 22:22:31 -0500

    Typing through Listing 3.5

--- src/erl34_app.erl
+++ src/erl34_app.erl
@@ -1,11 +1,67 @@
+%%%-------------------------------------------------------------------
+%%% @author Martin & Eric <erlware-dev@googlegroups.com>
+%%%  [http://www.erlware.org]
+%%% @copyright 2008-2010 Erlware
+%%% @doc RPC over TCP server. This module defines a server process that
+%%%      listens for incoming TCP connections and allows the user to
+%%%      execute RPC commands via that TCP stream.
+%%% @end
+%%%--------------------------------------------------------------------
 -module(erl34_app).
--behaviour(application).
 
--export([start/2]).
--export([stop/1]).
+-behaviour(gen_server).
 
-start(_Type, _Args) ->
-  erl34_sup:start_link().
+%% API
+-export([
+         start_link/1,
+         start_link/0,
+         get_count/0,
+         stop/0
+        ]).
 
-stop(_State) ->
-  ok.
+%% gen_server callbacks
+-export([
+         init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3
+        ]).
+
+-define(SERVER, ?MODULE).
+-define(DEFAULT_PORT, 1055).
+
+start_link() ->
+    start_link(?DEFAULT_PORT).
+
+start_link(Port) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
+
+get_count() ->
+    gen_server:call(?SERVER, get_count).
+
+stop() ->
+    gen_server:cast(?SERVER, stop).
+
+-record(state, {port, lsock, request_count = 0}).
+
+init([Port]) ->
+    {ok, LSock} = gen_tcp:listen(Port, [{active, true}]),
+    {ok, #state{port = Port, lsock = LSock}, 0}.
+
+handle_call(get_count, _From, State) ->
+    {reply, {ok, State#state.request_count}, State}.
+
+handle_cast(stop, State) ->
+     {stop, normal, State}.
+
+% FIXME STARTHERE in Listing 3.5
+handle_info(_Info, State) ->
+     {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
