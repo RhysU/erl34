21ebd071a7d5b2e396ed8c464ae4a07f9aedcee9
Rhys Ulerich <rhys.ulerich@gmail.com>
2016-12-03 17:35:38 -0500

    Finish typing through Listing 3.6

--- src/erl34_app.erl
+++ src/erl34_app.erl
@@ -2,35 +2,25 @@
 %%% @author Martin & Eric <erlware-dev@googlegroups.com>
 %%%  [http://www.erlware.org]
 %%% @copyright 2008-2010 Erlware
-%%% @doc RPC over TCP server. This module defines a server process that
-%%%      listens for incoming TCP connections and allows the user to
-%%%      execute RPC commands via that TCP stream.
+%%% @doc RPC over TCP server:  accepts incoming TCP connections and
+%%%      allows the execution of RPC commands from that stream.
 %%% @end
 %%%--------------------------------------------------------------------
 -module(erl34_app).
 
 -behaviour(gen_server).
 
-%% API
--export([
-         start_link/1,
-         start_link/0,
-         get_count/0,
-         stop/0
-        ]).
+%% API (Listing 3.2 on page 103)
+-export([ start_link/1, start_link/0, get_count/0, stop/0 ]).
 
-%% gen_server callbacks
--export([
-         init/1,
-         handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3
-        ]).
+%% gen_server callbacks (Listing 3.2 on page 103)
+-export([ init/1, handle_call/3, handle_cast/2,
+          handle_info/2, terminate/2, code_change/3 ]).
 
 -define(SERVER, ?MODULE).
 -define(DEFAULT_PORT, 1055).
+
+%% API (Listing 3.2 on page 103)
 
 start_link() ->
     start_link(?DEFAULT_PORT).
@@ -46,6 +36,8 @@ stop() ->
 
 -record(state, {port, lsock, request_count = 0}).
 
+%% gen_server callbacks (Listing 3.4 on page 110)
+
 init([Port]) ->
     {ok, LSock} = gen_tcp:listen(Port, [{active, true}]),
     {ok, #state{port = Port, lsock = LSock}, 0}.
@@ -56,12 +48,43 @@ handle_call(get_count, _From, State) ->
 handle_cast(stop, State) ->
      {stop, normal, State}.
 
-% FIXME STARTHERE in Listing 3.5
-handle_info(_Info, State) ->
-     {noreply, State}.
+%% gen_server callbacks (Listing 3.5 on page 113)
+
+handle_info({tcp, Socket, RawData}, State) ->
+    do_rpc(Socket, RawData),
+    RequestCount = State#state.request_count,
+    {noreply, State#state{request_count = RequestCount + 1}};
+handle_info(timeout, #state{lsock = LSock} = State) ->
+    {ok, _Sock} = gen_tcp:accept(LSock),
+    {noreply, State}.
 
 terminate(_Reason, _State) ->
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+%% Internal (Listing 3.6 on page 114)
+
+do_rpc(Socket, RawData) ->
+    try
+        {M, F, A} = split_out_mfa(RawData),
+        Result = apply(M, F, A),
+        gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Result]))
+    catch
+        _Class:Err ->
+            gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Err]))
+    end.
+
+split_out_mfa(RawData) ->
+    MFA = re:replace(RawData, "\r\n$", "", [{return, list}]),
+    {match, [M, F, A]} =
+        re:run(MFA,
+               "(.*):(.*)\s*\\((.*)\s*\\)\s*.\s*$",
+               [{capture, [1,2,3], list}, ungreedy]),
+    {list_to_atom(M), list_to_atom(F), args_to_terms(A)}.
+
+args_to_terms(RawArgs) ->
+    {ok, Toks, _Line} = erl_scan:string("[" ++ RawArgs ++ "]. ", 1),
+    {ok, Args} = erl_parse:parse_term(Toks),
+    Args.
