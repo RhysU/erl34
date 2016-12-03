455cfffa06360c6ca402de8d68e0340ebc6a3a64
Rhys Ulerich <rhys.ulerich@gmail.com>
2016-12-03 17:50:16 -0500

    Typing but busted

--- src/erl34_app.erl
+++ src/erl34_app.erl
@@ -1,90 +1,17 @@
-%%%-------------------------------------------------------------------
-%%% @author Martin & Eric <erlware-dev@googlegroups.com>
-%%%  [http://www.erlware.org]
-%%% @copyright 2008-2010 Erlware
-%%% @doc RPC over TCP server:  accepts incoming TCP connections and
-%%%      allows the execution of RPC commands from that stream.
-%%% @end
-%%%--------------------------------------------------------------------
--module(erl34_app).
+%% Listing 4.2 on page 124
+-module(elr34_app).
 
--behaviour(gen_server).
+-behavior(application).
 
-%% API (Listing 3.2 on page 103)
--export([ start_link/1, start_link/0, get_count/0, stop/0 ]).
+-export([ start/2, stop/1 ]).
 
-%% gen_server callbacks (Listing 3.2 on page 103)
--export([ init/1, handle_call/3, handle_cast/2,
-          handle_info/2, terminate/2, code_change/3 ]).
-
--define(SERVER, ?MODULE).
--define(DEFAULT_PORT, 1055).
-
-%% API (Listing 3.2 on page 103)
-
-start_link() ->
-    start_link(?DEFAULT_PORT).
-
-start_link(Port) ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
-
-get_count() ->
-    gen_server:call(?SERVER, get_count).
-
-stop() ->
-    gen_server:cast(?SERVER, stop).
-
--record(state, {port, lsock, request_count = 0}).
-
-%% gen_server callbacks (Listing 3.4 on page 110)
-
-init([Port]) ->
-    {ok, LSock} = gen_tcp:listen(Port, [{active, true}]),
-    {ok, #state{port = Port, lsock = LSock}, 0}.
-
-handle_call(get_count, _From, State) ->
-    {reply, {ok, State#state.request_count}, State}.
-
-handle_cast(stop, State) ->
-     {stop, normal, State}.
-
-%% gen_server callbacks (Listing 3.5 on page 113)
-
-handle_info({tcp, Socket, RawData}, State) ->
-    do_rpc(Socket, RawData),
-    RequestCount = State#state.request_count,
-    {noreply, State#state{request_count = RequestCount + 1}};
-handle_info(timeout, #state{lsock = LSock} = State) ->
-    {ok, _Sock} = gen_tcp:accept(LSock),
-    {noreply, State}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-%% Internal (Listing 3.6 on page 114)
-
-do_rpc(Socket, RawData) ->
-    try
-        {M, F, A} = split_out_mfa(RawData),
-        Result = apply(M, F, A),
-        gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Result]))
-    catch
-        _Class:Err ->
-            gen_tcp:send(Socket, io_lib:fwrite("~p~n", [Err]))
+start(_Type, _StartArgs) ->
+    case tr_sup:start_link() of
+        {ok, Pid} ->
+            {ok, Pid};
+        Other ->
+            {error, Other}
     end.
 
-split_out_mfa(RawData) ->
-    MFA = re:replace(RawData, "\r\n$", "", [{return, list}]),
-    {match, [M, F, A]} =
-        re:run(MFA,
-               "(.*):(.*)\s*\\((.*)\s*\\)\s*.\s*$",
-               [{capture, [1,2,3], list}, ungreedy]),
-    {list_to_atom(M), list_to_atom(F), args_to_terms(A)}.
-
-args_to_terms(RawArgs) ->
-    {ok, Toks, _Line} = erl_scan:string("[" ++ RawArgs ++ "]. ", 1),
-    {ok, Args} = erl_parse:parse_term(Toks),
-    Args.
+stop(_State) ->
+    ok.
--- src/erl34_sup.erl
+++ src/erl34_sup.erl
@@ -1,12 +1,23 @@
+%% Listing 4.3 on page 126, absent a spurious define
+
 -module(erl34_sup).
+
 -behaviour(supervisor).
 
+%% API
 -export([start_link/0]).
+
+%% Supervisor callbacks
 -export([init/1]).
 
+-define(SERVER, ?MODULE).
+
 start_link() ->
-  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 init([]) ->
-  Procs = [],
-  {ok, {{one_for_one, 1, 5}, Procs}}.
+    Server = {erl34_server, {erl34_server, start_link, []},
+             permanent, 2000, worker, [tr_server]},
+    Children = [Server],
+    RestartStrategy = {one_for_one, 0, 1},
+    {ok, {RestartStrategy, Children}}.
